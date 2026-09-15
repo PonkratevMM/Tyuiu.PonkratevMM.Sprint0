944d8d885b33fd2af3414c8ae9229ca0240b1768
PonkratevMM <matvey190808@gmail.com>
2026-09-15 22:24:39 +0300

    Task1

---
+++ Tyuiu.PonkratevMM.Sprint1.Task1.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Матвей";
+int age = 18;
+Console.WriteLine("Здравстуйте, меня зовут " + name + " мне " + age + " лет!");
+
+Console.ReadKey();
+  
