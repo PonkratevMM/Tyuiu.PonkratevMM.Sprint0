33f8aeb0f80a9d38affb6189e504a1931b4f8c61
PonkratevMM <matvey190808@gmail.com>
2026-09-15 23:21:44 +0300

    Task2

---
+++ Tyuiu.PonkratevMM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PonkratevMM.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PonkratevMM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Матвей";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет123, Матвей", res);
+
+        }
+    }
+}
---
+++ Tyuiu.PonkratevMM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.PonkratevMM.Sprint0.Task2.V0.Lib;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(DataService.GetMessage("Матвей"));
+Console.ReadKey();
