89d9fa17a74c75db5fb99640a9a35e845842c46f
PonkratevMM <matvey190808@gmail.com>
2026-09-16 19:12:42 +0300

    Task4

---
+++ Tyuiu.PonkratevMM.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.PonkratevMM.Sprint0.Task4.V0.Lib;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Subtraction(15, 5));
+Console.WriteLine(DataService.Multiplication(10, 10));
+Console.WriteLine(DataService.Division(5, 5));
+Console.ReadKey();
